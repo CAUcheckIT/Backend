988de76ba31f7237eba6bd6d27ff28ef3da58eed
임동재 <djlim1998@gmail.com>
2024-10-09 17:31:57 +0900

    Feat: Initial dev branch setup

---
+++ Backend/com.checkIT/src/test/java/checkIT/com/checkIT/ApplicationTests.java
@@ -0,0 +1,13 @@
+package checkIT.com.checkIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
