9094e4657368dfc0c07ae873e1434a70bd0cfc6b
gaeul3303 <rkdmfghd@naver.com>
2024-11-29 18:04:20 +0900

    reorganize project structure to move files

---
+++ src/main/java/capstone/checkIT/entity/Pin.java
@@ -0,0 +1,36 @@
+package capstone.checkIT.entity;
+
+import capstone.checkIT.entity.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@DynamicUpdate
+@DynamicInsert
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Pin extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false)
+    private Timestamp time;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="location_id")
+    private Location location;
+
+
+}
---
+++ src/main/java/capstone/checkIT/entity/TodoAlarm.java
@@ -0,0 +1,31 @@
+package capstone.checkIT.entity;
+
+import capstone.checkIT.entity.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@DynamicUpdate
+@DynamicInsert
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class TodoAlarm extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(columnDefinition = "TEXT")
+    @Lob
+    private String contents;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="member_id")
+    private Member member;
+}
