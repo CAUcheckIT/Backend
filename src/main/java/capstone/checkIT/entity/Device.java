b60c93db6bffc62e567d552e95405b25b43b3bf8
임동재 <djlim1998@gmail.com>
2024-12-07 23:04:07 +0900

    feat: Device entity 수정

--- src/main/java/capstone/checkIT/entity/Device.java
+++ src/main/java/capstone/checkIT/entity/Device.java
@@ -31,9 +31,7 @@ public class Device extends BaseEntity {
 
     @Column(nullable = false)
     private Boolean isLogin;
-
-    @Column(nullable=false)
-    private Boolean isDevice;
+    
 
     @ManyToOne(fetch=FetchType.LAZY)
     @JoinColumn(name="member_id")
